70656a2a103a94af961231c8b068f86208f4457e
Your Name <ermolaeva@agent.ru>
2017-04-12 16:53:43 +1000

    added base url

--- app/controllers/ticket.py
+++ app/controllers/ticket.py
@@ -3,17 +3,19 @@ from flask import request as req
 import requests
 
 
+base_url = 'https://ermolaeva.tania@gmail.com:12345@tatiana-ermolaeva.zendesk.com/api/v2'
+
+
 @app.route('/tickets/<int:id>')
 def get(id):
-    res = requests.get('https://ermolaeva.tania@gmail.com:12345@tatiana-ermolaeva.zendesk.com/api/v2/tickets/{0}.json'.format(id))
+    res = requests.get('{0}/tickets/{1}.json'.format(base_url, id))
+
     return res.text
 
 
 @app.route('/tickets')
 def list():
-    res = requests.get('https://ermolaeva.tania@gmail.com:12345@tatiana-ermolaeva.zendesk.com/api/v2/tickets.json', req.args)
+    res = requests.get('{0}/tickets.json'.format(base_url), req.args)
     return res.text
-
-
 
 
