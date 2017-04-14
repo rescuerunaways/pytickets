b43010130815b0ba4c0bae3ba95deaf81c4c100d
Your Name <ermolaeva@agent.ru>
2017-04-14 23:56:09 +1000

    eliminated shadowing vars

--- app/controllers/ticket.py
+++ app/controllers/ticket.py
@@ -1,21 +1,20 @@
-from app import app
-from flask import request as req
 import requests
+from flask import request as req
 
+from app import app
+from app.services.processor import process_one, process_list
 
 base_url = 'https://ermolaeva.tania@gmail.com:12345@tatiana-ermolaeva.zendesk.com/api/v2'
 
 
-@app.route('/tickets/<int:id>')
-def get(id):
-    res = requests.get('{0}/tickets/{1}.json'.format(base_url, id))
-
-    return res.text
+@app.route('/tickets/<int:n>')
+def get(n):
+    res = requests.get('{0}/tickets/{1}.json'.format(base_url, n))
+    print(res.status_code)
+    return process_one(res.text)
 
 
 @app.route('/tickets')
-def list():
+def get_list():
     res = requests.get('{0}/tickets.json'.format(base_url), req.args)
-    return res.text
-
-
+    return process_list(res.text)
