c735ad726a9e86348f59b7ec652cff8c7dabd116
Your Name <ermolaeva@agent.ru>
2017-04-15 20:59:53 +1000

    updated error handlers

--- app/controllers/ticket.py
+++ app/controllers/ticket.py
@@ -2,18 +2,21 @@ import requests
 from flask import request as req
 
 from app import app
+from app.errors.err_handlers import is_ok
 from app.services.processor import process_one, process_list
 
 base_url = 'https://ermolaeva.tania@gmail.com:12345@tatiana-ermolaeva.zendesk.com/api/v2'
 
 
 @app.route('/tickets/<int:n>')
-def get(n):
+def get_one(n):
     res = requests.get('{0}/tickets/{1}.json'.format(base_url, n))
+    is_ok(res)
     return process_one(res.text)
 
 
 @app.route('/tickets')
 def get_list():
     res = requests.get('{0}/tickets.json'.format(base_url), req.args)
+    is_ok(res)
     return process_list(res.text)
--- app/errors/err_handlers.py
+++ app/errors/err_handlers.py
@@ -1,19 +1,19 @@
 from app import app
 
 
-@app.errorhandler(401)
-def not_found(error):
-    app.logger.error('Server Error: %s', (error))
-    return "Could not authenticate user"
+def is_ok(res):
+    if not res.ok:
+        if 'application/json' in res.headers.get('Content-Type'):
+            raise Exception(res.reason, res.status_code, res.json())
+        else:
+            raise Exception(res.reason, res.status_code)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    app.logger.error('Server Error: %s', (error))
-    return "File not found"
+    return "Server error:{0}".format(Exception(error))
 
 
 @app.errorhandler(Exception)
 def server_error(error):
-    app.logger.error('Server Error: %s', (error))
-    return "Internal server error"
+    return "Server error:{0}".format(error)
