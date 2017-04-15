d3b3bf454d465e8e7a7c61c69bdfc6586ccec82b
Your Name <ermolaeva@agent.ru>
2017-04-15 19:43:47 +1000

    updated test to call mocks

--- app/tests/test_tickets.py
+++ app/tests/test_tickets.py
@@ -1,20 +1,19 @@
 from json import loads
+from unittest.mock import MagicMock as Mock
 from unittest.mock import patch
 
 from nose.tools import assert_list_equal
 
-from app.services.processor import process_one
+from app.controllers.ticket import get_one
 
 
 def mock(m):
-    with open("pytickets/app/tests/mocks/tickets/{0}.json".format(m)) as mock:
+    with open("app/tests/mocks/tickets/{0}.json".format(m)) as mock:
         return mock.read()
 
 
 @patch('app.controllers.ticket.requests.get')
-def test_process_one(mock_get):
-    raw_ticket = mock("raw/ticket")
-    processed_ticket = process_one(raw_ticket)
-    ticket = mock("ticket")
-
-    assert_list_equal(loads(processed_ticket), loads(ticket))
+def test_get_one(mock_api_call):
+    mock_api_call.return_value = Mock(status_code=200, text=mock("raw/ticket"))
+    response = get_one()
+    assert_list_equal(loads(mock("ticket")), loads(response));
